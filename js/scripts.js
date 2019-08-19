2c0cbc7d68dcc0957fb31c16c6a859950176313e
Student <example@example.com>
2019-08-19 13:45:10 -0700

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,37 @@
 $(document).ready(function() {
+  
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
-    $('li').click(function() {
-      $("ul#user").children("li").first().click(function() {
-  $(this).remove();
-});
-$("ul#webpage").children("li").first().click(function() {
-  $(this).remove();
-});
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
     });
   });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
-    $('li').click(function() {
-    alert('hi');
     $("ul#user").children("li").first().click(function() {
-      $("ul#user").children("li").first().click(function() {
-  $(this).remove();
-});
-$("ul#webpage").children("li").first().click(function() {
-  $(this).remove();
-});
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
-});
-
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
-    $('li').click(function() {
-      $("ul#user").children("li").first().click(function() {
-  $(this).remove();
-});
-$("ul#webpage").children("li").first().click(function() {
-  $(this).remove();
-});
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
     });
   });
+
+
 });
